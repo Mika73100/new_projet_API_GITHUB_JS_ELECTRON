86aa935f02d4fb402c83cc082f84e015562966fb
Micka73100 <Michael-73@live.Fr>
2023-02-08 20:50:50 +0100

    mise a jour ajour commentaire

--- preload.js
+++ preload.js
@@ -1,3 +1,18 @@
+// preload.js
+
+// Toutes les API Node.js sont disponibles dans le processus de préchargement.
+// Il a la même sandbox qu'une extension Chrome.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
+
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+})
+
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('darkMode', {
